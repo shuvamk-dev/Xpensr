0736022ecccc414679962adac4e54fae7c4c8b65
shuvamk <kshuvam564@gmail.com>
2021-07-30 23:29:50 +0530

    Created form functions

--- src/components/add-form/index.js
+++ src/components/add-form/index.js
@@ -5,15 +5,44 @@ import "./add-form.css";
 const AddForm = () => {
   const [categoryOpen, setCategoryOpen] = useState(false);
   const cat = categories;
+  const [title, setTitle] = useState("");
+  const [amount, setAmount] = useState("");
+  const [category, setCategory] = useState();
+
+  const handleTitle = (e) => {
+    setTitle(e.target.value);
+  };
+  const handleAmount = (e) => {
+    const val = parseFloat(e.target.value);
+    if (isNaN(val)) {
+      setAmount("");
+      return;
+    }
+    setAmount(val);
+  };
+  const handleCategory = (category) => {
+    setCategory(category);
+    setCategoryOpen(false);
+    console.log(category);
+  };
   return (
     <div className="add-form">
       <div className="form-item">
         <label>Title</label>
-        <input placeholder="Give a name to your expenditure" />
+        <input
+          placeholder="Give a name to your expenditure"
+          value={title}
+          onChange={(e) => handleTitle(e)}
+        />
       </div>
       <div className="form-item">
         <label>Amount ₹</label>
-        <input placeholder="Enter Amount" className="amount-input" />
+        <input
+          placeholder="Enter Amount"
+          className="amount-input"
+          onChange={(e) => handleAmount(e)}
+          value={amount}
+        />
       </div>
       <div className="category-container-parent">
         <div className="category">
@@ -21,7 +50,7 @@ const AddForm = () => {
             className="category-dropdown"
             onClick={() => setCategoryOpen(!categoryOpen)}
           >
-            <label>Category</label>
+            <label>{category ? category.title : "Category"}</label>
             <i class="fi-rr-angle-down"></i>
           </div>
           {categoryOpen && (
@@ -30,6 +59,8 @@ const AddForm = () => {
                 <div
                   className="category-item"
                   style={{ borderRight: `5px solid ${category.color}` }}
+                  key={category.id}
+                  onClick={() => handleCategory(category)}
                 >
                   <label>{category.title}</label>
                   <img src={category.icon.default} alt={category.title} />
