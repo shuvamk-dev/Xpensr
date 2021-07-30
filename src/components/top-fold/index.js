deb25cb915e510feb3c8325c8f7cb478a8410e53
shuvamk <kshuvam564@gmail.com>
2021-07-30 18:54:51 +0530

    Added top section

--- src/components/top-fold/index.js
+++ src/components/top-fold/index.js
@@ -1,7 +1,18 @@
 import React from "react";
 import "./top-fold.css";
 const TopFold = () => {
-  return <div>This is topfold</div>;
+  return (
+    <div className="topfold">
+      <div className="searchbar">
+        <i class="fi-rr-search"></i>
+        <input placeholder="Search for expenses" />
+      </div>
+      <div className="add-button">
+        <i class="fi-rr-add"></i>
+        <label>Add</label>
+      </div>
+    </div>
+  );
 };
 
 export default TopFold;
