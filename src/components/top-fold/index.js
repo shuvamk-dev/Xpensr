f09bf54217764c193bd00e0b3fbd4cb7ddac3ebd
shuvamk <kshuvam564@gmail.com>
2021-07-30 19:34:21 +0530

    Created topfold for both

--- src/components/top-fold/index.js
+++ src/components/top-fold/index.js
@@ -1,16 +1,33 @@
 import React from "react";
 import "./top-fold.css";
 const TopFold = () => {
+  const page = window.location.pathname;
+
   return (
     <div className="topfold">
-      <div className="searchbar">
-        <i class="fi-rr-search"></i>
-        <input placeholder="Search for expenses" />
-      </div>
-      <div className="add-button">
-        <i class="fi-rr-add"></i>
-        <label>Add</label>
-      </div>
+      {!page === "/" ? (
+        <div className="home-topfold">
+          <div className="searchbar">
+            <i class="fi-rr-search"></i>
+            <input placeholder="Search for expenses" />
+          </div>
+          <div className="add-button">
+            <i class="fi-rr-add"></i>
+            <label>Add</label>
+          </div>
+        </div>
+      ) : (
+        <div className="add-topfold">
+          <div className="add-topfold-button">
+            <i class="fi-rr-angle-left"></i>
+            <label>Back</label>
+          </div>
+          <div className="add-topfold-button">
+            <i class="fi-rr-cross-circle"></i>
+            <label>Cancel</label>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
---
+++ src/pages/add-expense/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./add-expense.css";
+const AddExpense = () => {
+  return <div>Thsi si add expense page</div>;
+};
+
+export default AddExpense;
